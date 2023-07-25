0a42f1aecaf0ad2b8bb3121a07d4511bc0645cfb
reiskaren0228 <83614778+reiskaren0228@users.noreply.github.com>
2023-07-25 14:41:12 +0000

    tarefa_casa

--- aula_20_07_part2.js
+++ aula_20_07_part2.js
@@ -68,19 +68,102 @@
 //     }
 // }num()
 
+//Classificar um número em três categorias: negativo, zero ou positivo:
 
-function  num(){
-    num=10;
-
-    if(num % 2===0){
-        console.log(`O ${num} é divisivel por 2`);
+function classificarNumero(num) {
+    if (num < 0) {
+      return "Negativo";
+    } else if (num === 0) {
+      return "Zero";
+    } else {
+      return "Positivo";
     }
-}
+  }
 
-function nome(){
-    nome="karen";
-    nome=true;
-    nome=10;
+  console.log(classificarNumero(-28)); 
+  console.log(classificarNumero(0));  
+  console.log(classificarNumero(20)); 
+  
 
-    if(typeof nome === "boolean")
-}
+  //Verificar se um número é divisível por 2, 3 ou 5:
+
+  function verificarDivisibilidade(num) {
+    let divisivelPor2 = num % 2 === 0;
+    let divisivelPor3 = num % 3 === 0;
+    let divisivelPor5 = num % 5 === 0;
+  
+    return {
+      divisivelPor2: divisivelPor2,
+      divisivelPor3: divisivelPor3,
+      divisivelPor5: divisivelPor5,
+    };
+  }
+
+  console.log(verificarDivisibilidade(18)); 
+  console.log(verificarDivisibilidade(12)); 
+  console.log(verificarDivisibilidade(50));  
+
+  
+  //Classificar uma variável em 4 categorias: booleana, numérica, string ou indefinida:
+
+  function classificarVariavel(valor) {
+    if (typeof valor === "boolean") {
+      return "Booleana";
+    } else if (typeof valor === "number") {
+      return "Numérica";
+    } else if (typeof valor === "string") {
+      return "String";
+    } else {
+      return "Indefinida";
+    }
+  }
+ 
+  console.log(classificarVariavel(true));    
+  console.log(classificarVariavel(2));      
+  console.log(classificarVariavel("karen")); 
+  console.log(classificarVariavel(null));  
+  
+  //Classificar um número em 5 categorias: negativo, zero, positivo, par ou ímpar:,
+
+  function classificarNumeroDetalhado(num) {
+    if (num < 0) {
+      return "Negativo";
+    } else if (num === 0) {
+      return "Zero";
+    } else {
+      let categoria = "Positivo";
+      if (num % 2 === 0) {
+        categoria += ", Par";
+      } else {
+        categoria += ", Ímpar";
+      }
+      return categoria;
+    }
+  }
+  
+  console.log(classificarNumeroDetalhado(-35)); 
+  console.log(classificarNumeroDetalhado(0)); 
+  console.log(classificarNumeroDetalhado(12));
+  console.log(classificarNumeroDetalhado(53));  
+  
+
+  //Verificar se um número é divisível por 2, 3, 5 ou 7:
+  
+
+  function verificarDivisibilidade2(num) {
+    let divisivelPor2 = num % 2 === 0;
+    let divisivelPor3 = num % 3 === 0;
+    let divisivelPor5 = num % 5 === 0;
+    let divisivelPor7 = num % 7 === 0;
+  
+    return {
+      divisivelPor2: divisivelPor2,
+      divisivelPor3: divisivelPor3,
+      divisivelPor5: divisivelPor5,
+      divisivelPor7: divisivelPor7,
+    };
+  }
+ 
+  console.log(verificarDivisibilidade2(12)); 
+  console.log(verificarDivisibilidade2(56)); 
+  console.log(verificarDivisibilidade2(7));  
