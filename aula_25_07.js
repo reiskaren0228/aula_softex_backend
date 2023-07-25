5bb9cd301a4c865d2d8af6d245f4a35533456f5e
reiskaren0228 <83614778+reiskaren0228@users.noreply.github.com>
2023-07-25 17:47:48 +0000

    add atualização

--- aula_25_07.js
+++ aula_25_07.js
@@ -2,12 +2,13 @@ const readline = require("readline-sync");
 
 //Permissão para dirigir
 function dirigir(drive) {
-    var drive = readline.question("Digite sua idade: ");
+    
+    let drive = readline.question("Digite sua idade: ");
 
    if (drive < 16) {
      return "Você não pode dirirgir!!!";
    } else if (drive >= 16 && drive <= 17) {
-        return "Falta pouco, vai apredendo a dirigir com seus pais!!!";
+        return "Você já pode pedir autorização dos seus pais para dirigir!!!";
    } else if (drive >= 18 && drive <= 70) {
         return "Parabéns Você já pode dirigir!!";
    } else {
