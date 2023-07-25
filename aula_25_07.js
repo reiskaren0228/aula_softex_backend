92eb8af7e761a636f0de07936d9c5f397250ac85
reiskaren0228 <83614778+reiskaren0228@users.noreply.github.com>
2023-07-25 18:27:05 +0000

    add cod

--- aula_25_07.js
+++ aula_25_07.js
@@ -1,17 +1,115 @@
 const readline = require("readline-sync");
 
 //Permissão para dirigir
-function dirigir(drive) {
+// function dirigir(drive) {
 
-    var drive = readline.question("Digite sua idade: ");
+//     var drive = readline.question("Digite sua idade: ");
 
-   if (drive < 16) {
-     return "Você não pode dirirgir!!!";
-   } else if (drive >= 16 && drive <= 17) {
-        return "Você já pode pedir autorização dos seus pais para dirigir!!!";
-   } else if (drive >= 18 && drive <= 70) {
-        return "Parabéns Você já pode dirigir!!";
-   } else {
-        return "Você já está bem idoso, dirija com algum acompanhante.";
-   }
-   } console.log(dirigir())
+//    if (drive < 16) {
+//      return "Você não pode dirirgir!!!";
+//    } else if (drive >= 16 && drive <= 17) {
+//         return "Você já pode pedir autorização dos seus pais para dirigir!!!";
+//    } else if (drive >= 18 && drive <= 70) {
+//         return "Parabéns Você já pode dirigir!!";
+//    } else {
+//         return "Você já está bem idoso, dirija com algum acompanhante.";
+//    }
+//    } console.log(dirigir())
+
+//Exercicio 1:
+
+
+// let diaSemana = 8;
+
+// switch(diaSemana) {
+
+//     case 1:
+//         console.log("O dia da semana escolhido é o Domingo.");
+//         break;
+    
+//     case 2:
+//         console.log("O dia da semana escolhido é a Segunda-feira");
+//         break;
+    
+//     case 3:
+//         console.log("O dia da semana escolhido é a Terça-feira");
+//         break;  
+        
+//     case 4:
+//         console.log("O dia da semana escolhido é a Quarta-feira");
+//         break;  
+    
+//     case 5:
+//         console.log("O dia da semana escolhido é a Quinta-feira");
+//         break;  
+
+//     case 6:
+//         console.log("O dia da semana escolhido é a Sexta-feira");
+//         break;  
+
+//     case 7:
+//         console.log("O dia da semana escolhido é a Sábado");
+//         break;  
+
+//     default:
+//         console.log("Digite de 1 até o número 7") 
+//     }
+   
+//Exercicio 2:
+
+
+let mês = 8;
+
+switch(mês) {
+
+    case 1:
+        console.log("O mês escolhido é Janeiro.");
+        break;
+    
+    case 2:
+        console.log("O mês escolhido é Fevereiro.");
+        break;
+    
+    case 3:
+        console.log("O mês escolhido é Março.");
+        break;  
+        
+    case 4:
+        console.log("O mês escolhido é Abril.");
+        break;  
+    
+    case 5:
+        console.log("O mês escolhido é Maio.");
+        break;  
+
+    case 6:
+        console.log("O mês escolhido é Junho.");
+        break;  
+
+    case 7:
+        console.log("O mês escolhido é Julho.");
+        break;
+
+    case 8:
+         console.log("O mês escolhido é Agosto.");
+         break; 
+
+    case 9:
+         console.log("O mês escolhido é Setembro.");
+         break;  
+
+    case 10:
+          console.log("O mês escolhido é Outubro.");
+          break;  
+
+    case 11:
+          console.log("O mês escolhido é Novembro.");
+          break;  
+
+    case 12:
+          console.log("O mês escolhido é Dezembro.");
+          break;  
+
+    default:
+        console.log("Digite de 1 até o número 12") 
+    }
