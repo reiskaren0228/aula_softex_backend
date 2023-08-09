f03ef626b41d63c5369024020e1ff1323d1a341a
reiskaren0228 <83614778+reiskaren0228@users.noreply.github.com>
2023-08-09 18:25:04 +0000

    coreção tarefa aula

--- aula_08_08.js
+++ aula_08_08.js
@@ -15,69 +15,72 @@ const readline = require("readline-sync");
 // exibirSaudacao();
 
 //Atv 1
-function saudacao(nome){
-       nome = readline.question("Qual o seu nome? ")
-       console.log(`${nome}, Olá, Mundo!`)
+
+function saudacao() {
+       const nome = readline.question("Qual o seu nome? ");
+       console.log(`${nome}, Olá, Mundo!`);
 }
+
 saudacao();
 
-//Atv 2
-// function numeroDobro(num1,num2) {
-//     num1 = parseInt(readline.question("Digite um número: "));
-     
-//     multiplicao = num1 * 2;
 
-//     console.log(`O dobro desse número é ${multiplicao} `)
-// }
-// numeroDobro();
+//Atv 2
+// function numeroDobro(num) {
+//        num = parseInt(readline.question("Digite um número: "));
+
+//        const multiplicacao = num * 2;
+
+//        console.log(`O dobro do número ${num} é ${multiplicacao}`);
+//    }
+
+//    numeroDobro();
 
 //Atv 3
+
 // function mostrarNumeros(inicio, fim) {
-//     inicio = parseInt(readline.question("Digite um número do ínicio da contagem: "));
-//     fim = parseInt(readline.question("Digite o número do fim da contagem: "));
-//     mensagemInicio = "Iniciando a contagem ... ";
-//     mensagemFim = "Fim da contagem ... ";
+//        inicio = parseInt(readline.question("Digite um número do ínicio da contagem: "));
+//        fim = parseInt(readline.question("Digite o número do fim da contagem: "));
+//        mensagemInicio = "Iniciando a contagem ... ";
+//        mensagemFim = "Fim da contagem ... ";
 
-//     console.log(mensagemInicio)
+//        console.log(mensagemInicio)
 
-//     for(inicio=0; inicio <= fim; inicio++ )
-//     {
-//         console.log(`${inicio}`)
-//     }
+//        for(let i = inicio; i <= fim; i++ ) {
+//            console.log(`${i}`);
+//        }
 
-//      console.log(mensagemFim)
-// }
+//        console.log(mensagemFim)
+//    }
 
-// mostrarNumeros();
+//    mostrarNumeros();
+
 //Atv 4
 
 // function verificarPar(num) {
-//     num = parseInt(readline.question("Digite um número: "));
+//        num = parseInt(readline.question("Digite um número: "));
 
-//     if(num%2===0) {
-
-//         console.log(`O numero ${num} é par`);
-
-//     }else {
-
-//         console.log(`O numero ${num} é impar`);
-
-//     }
+//        if (num % 2 === 0) {
+//               console.log(`O número ${num} é par`);
+//        } else {
+//               console.log(`O número ${num} é ímpar`);
+//        }
 // }
+
 // verificarPar();
+
 //Atv 5
 
-// function imprimirLista(array) {
-
-//     array = [];
-
-//     for(let i=0; i < 5; i++){
-//         array.push(readline.question("Digite um número:  "));
-//     }
-//     console.log(array)
-
-// }
-  
-// imprimirLista();
-
-
+function imprimirLista() {
+       const array = [];
+   
+       for (let i = 0; i < 5; i++) {
+           array.push(readline.question("Digite um número: "));
+       }
+       
+       console.log("Elementos do array:");
+       for (const elemento of array) {
+           console.log(elemento);
+       }
+   }
+   
+   imprimirLista();
