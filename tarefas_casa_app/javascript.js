f76b3c702f6a9d552e95caf47a5b8fe44d9c974a
reiskaren0228 <83614778+reiskaren0228@users.noreply.github.com>
2023-08-14 21:45:13 +0000

    add

---
+++ tarefas_casa_app/javascript.js
@@ -0,0 +1,36 @@
+// Boolean
+let isTrue;
+let isFalse = false;
+
+// Bigint
+let bigNumber;
+let bigValue = BigInt(12345);
+
+// String
+let emptyString;
+let text = "Olá, Seja bem Vindos!";
+
+// Symbol
+let uniqueSymbol;
+let symbolValue = Symbol("Simbolos");
+
+// Exibir os conteúdos iniciais (undefined)
+console.log("Conteúdos iniciais:");
+console.log("isTrue:", isTrue);
+console.log("isFalse:", isFalse);
+console.log("bigNumber:", bigNumber);
+console.log("emptyString:", emptyString);
+console.log("uniqueSymbol:", uniqueSymbol);
+
+// Atribuir valores 
+bigNumber = BigInt(98765);
+emptyString = "";
+uniqueSymbol = Symbol("Simbolos conti...");
+
+// Exibir os valores
+console.log("\nVisualização após as atribuição:");
+console.log("isTrue:", isTrue);
+console.log("isFalse:", isFalse);
+console.log("bigNumber:", bigNumber);
+console.log("emptyString:", emptyString);
+console.log("uniqueSymbol:", uniqueSymbol);
