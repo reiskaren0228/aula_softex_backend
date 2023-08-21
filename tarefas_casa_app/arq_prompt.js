051ad6d63afafdb6fbf52148b4d7a4e2ded5705c
reiskaren0228 <83614778+reiskaren0228@users.noreply.github.com>
2023-08-21 22:07:25 +0000

    tarefa casa

---
+++ tarefas_casa_app/arq_prompt.js
@@ -0,0 +1,9 @@
+const prompt = require("prompt-sync")();
+
+let nome = prompt("Informe seu nome: ")
+
+
+console.log(nome);
+
+console.log("Aperte ENTER para fechar!");
+prompt()//evita q o prompt feche sozinho qndo fizer o atalho
