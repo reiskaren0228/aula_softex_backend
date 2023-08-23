8b2fda4fa85cf95aa957a6db8f36c28123c703c7
reiskaren0228 <83614778+reiskaren0228@users.noreply.github.com>
2023-08-23 21:40:54 +0000

    mediaALuno

---
+++ tarefas_casa_app/media_aluno.js
@@ -0,0 +1,33 @@
+const readline = require('readline-sync');
+
+const nota1 = parseFloat(readline.question("Digite a primeira nota do aluno: "));
+const nota2 = parseFloat(readline.question("Digite a segunda nota do aluno: "));
+const nota3 = parseFloat(readline.question("Digite a terceira nota do aluno: "));
+
+const media = (nota1 + nota2 + nota3) / 3;
+
+if (media >= 7 ) {
+    console.log("*** Aluno Aprovado***");
+    console.log((`A média do Aluno foi ${media.toFixed(1)}`));
+} else {
+    
+    console.log("*** Aluno Reprovado***");
+    console.log((`A média do Aluno foi ${media.toFixed(1)}`));
+}
+
+console.log("------------- // ----------------")
+
+function notaMinima(nota_1, nota_2) {
+    const mediaParaAprovacao = 7;
+    const somaNotas = nota_1 + nota_2;
+    const notaMinima = (mediaParaAprovacao * 3 - somaNotas) / 3;
+    return notaMinima;
+}
+
+
+const nota_1 = parseFloat(readline.question("Informe a primeira nota: "));
+const nota_2 = parseFloat(readline.question("Informe a segunda nota: "));
+
+const notaMinimaParaAprovacao = notaMinima(nota_1, nota_2);
+
+console.log(`Para atingir a média 7, o Aluno precisará tirar pelo menos ${notaMinimaParaAprovacao.toFixed(2)} na terceira avaliação.`);
