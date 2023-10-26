8747216e4151392cdc3755129a4c94aae8d036c8
reiskaren0228 <83614778+reiskaren0228@users.noreply.github.com>
2023-10-26 11:12:35 -0300

    Update banco.ts

--- aula_24_10/banco.ts
+++ aula_24_10/banco.ts
@@ -1,25 +1,40 @@
-import { log } from "console";
+export default class Banco {
+   
+    nome: string;
+    sobrenome: string;
+    contaAtiva: boolean;
 
-export class Banco {
-    public nome: string;
-    public sobrenome: string;
-    protected contaAtiva: boolean;
-
-    constructor(
-        nome: string,
-        sobrenome: string,
-        contaAtiva: boolean
-    ) {
-
-        this.nome = nome;
-        this.sobrenome = sobrenome;
+    constructor(clienteNome: string, sobNome: string, contaAtiva: boolean) {
+        this.nome = clienteNome;
+        this.sobrenome = sobNome;
         this.contaAtiva = contaAtiva;
+    }
+
+    public msgContaAtiva(): void {
+        if (this.contaAtiva === true) {
+            console.log(`*** ACESSO LIBERADO *** \n--> Conta Ativa no Sistema ** `);
+            console.log("------------------------------------------------------------");
+            
+        } else {
+            console.log(`*** ERROR - ACESSO NEGADO *** \n--> Conta Inativa no Sistema <--`);
+            console.log("------------------------------------------------------------");
+        }
+
     }
 }
 
-let banco = new Banco('Karen', 'Reis', true);
-console.log(banco);
+let cliente = new Banco('Karen', 'Reis', true);
 
+console.log(`\n *** STATUS BANCÁRIO *** \n
+-- Cliente: ${cliente.nome} ${cliente.sobrenome} 
+-- Conta Ativa: ${cliente.contaAtiva} \n`)
 
+cliente.msgContaAtiva()
 
+let cliente1 = new Banco('João', 'Silva', false);
+console.log(`\n *** STATUS BANCÁRIO *** \n
+-- Cliente: ${cliente1.nome} ${cliente1.sobrenome} 
+-- Conta Ativa: ${cliente1.contaAtiva} \n`);
+
+cliente1.msgContaAtiva()
 
