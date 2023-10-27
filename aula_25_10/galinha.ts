9dada0395f6a8515f66e53329ef9ccba11cc3957
reiskaren0228 <83614778+reiskaren0228@users.noreply.github.com>
2023-10-27 11:44:53 -0300

    Update galinha.ts

--- aula_25_10/galinha.ts
+++ aula_25_10/galinha.ts
@@ -3,6 +3,7 @@ import { IAnimal } from "./animal";
 
 export interface IGalinha extends IAnimal {
     qtdPatas: number;
+    interageVisita(): void; 
 }
 
 function patas() {
@@ -15,16 +16,20 @@ function patas() {
 
       
     }
-    galinha.interageVisita();
+   }
+
+class Galinha implements IGalinha {
+    constructor(public tipo: string, public nome: string, public idade: number, public porte: string, public qtdPatas: number) {
+    }
+
+    interageVisita() {
+        console.log(`A galinha ${this.nome} é muito dócil quando é acariciada`);
+    }
 }
 
-
-class interageVisita implements IAnimal {
-    constructor(public tipo: string, public nome: string, public idade: number, public porte: string) {
-        console.log(` A galinha ${nome} é muito dócil quando é acariciada`);
-    
-    }
-   
+function patas() {
+    const galinha: IGalinha = new Galinha('Ave', 'Cocoricó', 3, 'Pequeno', 2);
+    galinha.interageVisita();
 }
 
 patas();
