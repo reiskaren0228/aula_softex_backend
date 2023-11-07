e88bc6fcc54c914ab48d7d5ada3df67ba2e41edc
reiskaren0228 <83614778+reiskaren0228@users.noreply.github.com>
2023-11-07 18:44:00 +0000

    auala 7_11

---
+++ aula_07_11.ts
@@ -0,0 +1,15 @@
+export class Clinete {
+    constructor(
+        nome: string,
+        idade: number,
+        endereco: string
+    ) {
+
+    }
+}
+
+class Endereco {
+    constructo(rua: string, numero: number){
+        
+    }
+}
